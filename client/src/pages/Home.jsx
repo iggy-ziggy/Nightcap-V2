01f7b5d4c6a435d46a339662d091e9b0b6cf42c8
murpheycm <cmmurphey@gmail.com>
2023-10-31 18:54:07 -0400

    Updating home with Firebase

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,6 +10,24 @@ import ThoughtFeed from '../components/ThoughtFeed';
 import Auth from '../utils/auth';
 import img from '/profilePicture.png'
 
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+// Initialize Firebase with your Firebase configuration
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);

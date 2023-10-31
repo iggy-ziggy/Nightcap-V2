5e89c9e050dbed1b0a56855a5969651870ba079a
murpheycm <cmmurphey@gmail.com>
2023-10-30 20:50:42 -0400

    Updated home to have username render

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from '@apollo/client';
 import ThoughtFeed from '../components/ThoughtFeed';
 import { QUERY_THOUGHTS } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
 
   return (
+    
     <main>
+      <h2>Hey, <span>{Auth.getProfile().data.username}</span>!</h2>
       <div className="flex-row justify-center">
         
         <div className="col-12 col-md-8 mb-3">
@@ -16,7 +19,7 @@ const Home = () => {
           ) : (
             <ThoughtFeed
               thoughts={thoughts}
-              title="What Others are Drinking:"
+              title="See What Others are Drinking:"
             />
           )}
         </div>

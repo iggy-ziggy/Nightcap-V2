f695eb326e024513f522d1549395ac83185ec61f
murpheycm <cmmurphey@gmail.com>
2023-10-30 20:23:17 -0400

    Commit before switching branches

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,24 +12,7 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
-        <p>Content coming soon...</p>
-        <p>This is the home page, by the way...</p>
-        {/* <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
-        </div> */}
+      <friendThoughts thoughts={thoughts} title="Thoughts" />
       </div>
     </main>
   );

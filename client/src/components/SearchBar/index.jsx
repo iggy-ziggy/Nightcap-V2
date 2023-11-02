746ab61bcc8176248f6b8a1925502eb0a9a2536b
larsly <larissa.bierly@gmail.com>
2023-11-02 04:22:48 -0400

    search page

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -6,98 +6,106 @@ import { BiSearchAlt } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { Link } from "react-router-dom";
 
-function SearchBar({placeholder, data, handleFilteredData}) {
-    const [filteredData, setFilteredData] = useState([]);
-    const [wordEntered, setWordEntered] = useState("");
-    const NavigateTo = useNavigate();
+
+function SearchBar({ placeholder, data, handleFilteredData }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+  const NavigateTo = useNavigate();
 
 
-    const handleFilter = (event) => {
+  const handleFilter = (event) => {
 
-        const searchWord = event.target.value;
-        setWordEntered(searchWord);
-        console.log(searchWord);
-        const newFilter = data.filter((item) => {
-            return item.name && item.name.toString().toLowerCase().includes(searchWord.toString().toLowerCase());
-        });
-        if (searchWord === "") {
-            setFilteredData([]);    
-        } else {
-            setFilteredData(newFilter);
-            console.log(filteredData);
-            onFilter(newFilter);
-        }
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            handleSearch();
-        }
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    console.log(searchWord);
+    const newFilter = data.filter((item) => {
+      return item.name && item.name.toString().toLowerCase().includes(searchWord.toString().toLowerCase());
+    });
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+      console.log(filteredData);
+      onFilter(newFilter);
     }
-    const handleSearch = () => {
-        NavigateTo(`/search?q=${wordEntered}`);
-      };
-    const clearInput = () => {
-        setFilteredData([]);
-        setWordEntered("");
-    };
+  };
 
-    return (
-        <div className="search">
-          <div className="searchInputs">
-            <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter} onKeyDown={handleKeyDown} />
-            <div className="searchIcon">
-              {filteredData.length > 0 ? <BiSearchAlt onClick={handleSearch}/> : <AiOutlineClose id="clearBtn" onClick={clearInput} />}
-            </div>
-          </div>
-          {filteredData.length !== 0 && (
-            <div className="dataResult">
-              {filteredData.slice(0, 15).map((value, key) => {
-                const itemLink = value.type === "business" ? `/business/${value.id}` : `/profile/${value.id}`;
-                return (
-                  <Link to={itemLink} key={key}>
-                    <div className="data">
-                      <div>
-                        {value.image ? ( 
-                          <img className="dataImg" src={value.image} alt={value.name} />
-                        ) : (
-                          <img
-                            className="dataImg"
-                            src="https://placeholder.pics/svg/200x200/757575-ADADAD/FFFFFF/IMAGE%20UNAVAILABLE"
-                            alt="Placeholder"
-                          />
-                        )}
-                      </div>
-                      <div className="dataInfo">
-                        <div>{value.name}</div><div>{value.type}</div>
-                      </div>
-                    </div>
-                  </Link>
-                );
-              })}
-              <div className="links">
-                <Link to={`/search?q=${wordEntered}&type=business`}>
-                  View More Businesses
-                </Link>
-                <Link to={`/search?q=${wordEntered}&type=user`}>
-                  View More Users
-                </Link>
-              </div>
-            </div>
-          )}
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  }
+  const handleSearch = () => {
+    NavigateTo(`/search?q=${wordEntered}`);
+  };
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search justify-center items-center">
+      <div className="searchInputs">
+        <input type="text" className="Input" placeholder={placeholder} value={wordEntered} onChange={handleFilter} onKeyDown={handleKeyDown} />
+        <div className="searchIcon">
+          {filteredData.length > 0 ? <BiSearchAlt onClick={handleSearch} /> : <AiOutlineClose id="clearBtn" onClick={clearInput} />}
         </div>
-      );
-    }
+      </div>
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            const itemLink = value.type === "business" ? `/business/${value.id}` : `/profile/${value.id}`;
+            return (
+              <Link to={itemLink} key={key}>
+                <div className="data">
+                  <div>
+                    {value.image ? (
+                      <img className="dataImg" src={value.image} alt={value.name} />
+                    ) : (
+                      <img
+                        className="dataImg"
+                        src="https://placeholder.pics/svg/200x200/757575-ADADAD/FFFFFF/IMAGE%20UNAVAILABLE"
+                        alt="Placeholder"
+                      />
+                    )}
+                  </div>
+                  <div className="dataInfo">
+                    <div>{value.name}</div><div>{value.type}</div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+          <div className="links">
+            <Link
+              to={`/search?q=${wordEntered}&type=business`}
+              style={{
+                color: 'text-secondary',
+                hover: 'text-primary',
+              }}
+            >
+              View More Businesses
+            </Link>
+            <Link to={`/search?q=${wordEntered}&type=user`}>
+              View More Users
+            </Link>
+          </div>
+        </div>
+      )
+      }
+    </div >
+  );
+}
 
-    SearchBar.propTypes = {
-        placeholder: PropTypes.string.isRequired,
-        data: PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            image: PropTypes.string, // Allow null or string
-        })).isRequired,
-        handleFilter: PropTypes.func.isRequired,
-    };
+SearchBar.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string, // Allow null or string
+  })).isRequired,
+  handleFilter: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_BUSINESSES, QUERY_USERS } from '../utils/queries';
 import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { Navbar, SideNav } from '../components';
 
 function Search() {
   const [combinedData, setCombinedData] = useState([]);
@@ -52,9 +54,9 @@ function Search() {
     }
 
     return (
-      <div className="results">
+      <div className={`${styles.paddingX} max-w-7xl mx-auto relative z-0`}>
         {filteredData.map((item) => (
-          <Link key={item.id} to={`/${item.type}/${item.id}`} className="resultLink">
+          <Link key={item.id} to={`/${item.type}/${item.id}`} className="mt-12 flex-col flex gap-10 flex-[0.75] bg-black-100 p-8 rounded-2xl w-full hover:shadow-card">
             <div className="result">
               {item.image ? (
                 <img className="resultImg" src={item.image} alt={item.name} />
@@ -66,8 +68,8 @@ function Search() {
                 />
               )}
               <div className="resultInfo">
-                <div className="resultName">{item.name}</div>
-                <div className="resultType">{item.type}</div>
+                <div className={styles.sectionSubText}>{item.name}</div>
+                <div className="text-white font-medium mb-2">{item.type}</div>
               </div>
             </div>
           </Link>
@@ -80,11 +82,15 @@ function Search() {
   };
 
   return (
-    <div>
-      <div>
-        <SearchBar placeholder="Search for users or businesses..." data={combinedData} />
+    <div className='z-0 bg-primary'>
+      <Navbar />
+      <div className={`${styles.paddingX} bg-primary h-screen relative inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+        <SideNav />
+        <div className={`${styles.paddingX} max-w-7xl mx-auto z-0`}>
+          <SearchBar placeholder="Search for users or businesses..." data={combinedData} />
+          <div>{renderContent()}</div>
+        </div>
       </div>
-      <div>{renderContent()}</div>
     </div>
   );
 }
